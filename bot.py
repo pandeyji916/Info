25482acbb4139215b387b289c149b6db70beeba7
pandeyji916 <sonupandey010180@gmail.com>
2026-08-31 23:08:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,10 +17,10 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Bot, Me
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters, CallbackContext
 
 # ---------------- CONFIG ----------------
-BOT_TOKEN = "8625091227:AAFeBaRX04tHLWlbguh0w-9FoMbLrGZTC1o"
-BOT_USERNAME = "Camerabbbot"        # provided by you (no @)
-BUY_CREDITS_USERNAME = "trolex00" # contact username (no @)
-ADMIN_IDS = [7355745231]               # admin numeric IDs
+BOT_TOKEN = "8900586063:AAH79gfc7E6FVjrbsxl664YDMNYzuw_mJcM"
+BOT_USERNAME = "@nyxor_ji_bot"        # provided by you (no @)
+BUY_CREDITS_USERNAME = "FLEXSAMAY" # contact username (no @)
+ADMIN_IDS = [5385377266]               # admin numeric IDs
 
 # core settings
 REFERRAL_BONUS = 2
